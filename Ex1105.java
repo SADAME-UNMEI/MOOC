74b752a075fc25902016d0b89e16efad239c07b2
SADAME-UNMEI <31981500+SADAME-UNMEI@users.noreply.github.com>
2017-09-15 15:13:58 +0800

    Add files via upload

---
+++ Ex1105.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.*;
+
+public class Ex1105 {
+    public static void main(String args[]) {
+        double x=StdIn.readDouble();
+        double y=StdIn.readDouble();
+        StdOut.println(x>0 && x<1 &&y>0&&y<1);
+    }
+}
