b9fa0cd9a13e5b4a1de10605bf721ae0ce0af153
SADAME-UNMEI <31981500+SADAME-UNMEI@users.noreply.github.com>
2017-09-15 14:38:56 +0800

    Add files via upload

---
+++ Ex03.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.*;
+
+public class Ex03 {
+    public static void main(String args[]) {
+        int a=StdIn.readInt();
+        int b=StdIn.readInt();
+        int c=StdIn.readInt();
+        if(a==b && a==c)
+            StdOut.printf("equal\n");
+        else
+            StdOut.printf("not equal\n");
+    }
+}
+            
